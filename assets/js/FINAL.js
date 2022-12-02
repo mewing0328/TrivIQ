d429d3ddd00f67b0593a7fb4aee341347ee61a4d
MEwing <masandraewing@gmail.com>
2022-12-02 13:37:49 -0500

    Save safe point

--- assets/js/FINAL.js
+++ assets/js/FINAL.js
@@ -7,21 +7,25 @@ var choiceBEl = document.querySelector("#B");
 var choiceCEl = document.querySelector("#C");
 var choiceDEl = document.querySelector("#D");
 var nextBtnEl = document.querySelector("nextBtn");
-var highScoresEl = document.querySelector("#highScores")
+var highScoresEl = document.querySelector("#highScores");
+var highScoresListEl = document.querySelector(".highScoresList");
 var timerEl = document.querySelector(".timer-count");
 var currentScoreEl = document.querySelector("#currentScore");
-var resetBtnEl = document.querySelector(".resetButton");
+var saveBtnEl = document.querySelector(".saveButton");
+var tryAgainBtnEl = document.querySelector(".tryAgainButton");
+var clearHighScoresEl = document.querySelector(".clearHighScores");
+var viewAllHSEl = document.querySelector(".viewAllHighScores");
 var greetingEl = document.querySelector("#greeting");
 var myFormEl = document.querySelector("#myForm");
 var userInitialsEl = document.querySelector("#userInitials");
+var userInputEl = document.querySelector("#userInput");
 var responseEl = document.querySelector("#response");
 var person1El = document.querySelector("#person1");
-var viewHighScoreEl = document.querySelector(".highScoreButton");
 var pickedEl = document.querySelector("#picked");
 
 var theScore = 0;
 var userChoice = "";
-var secondsLeft = 50;
+var secondsLeft = 5;
 
 //TO DO: add a reset button option
 
@@ -51,7 +55,145 @@ var cards = [
         choiceDEl : "d",       
         answer : "A", 
     },
+    {   
+        questionEl : "INDEX 2: Question 3",
+        choiceAEl : "a",
+        choiceBEl : "b",
+        choiceCEl : "c",
+        choiceDEl : "d",       
+        answer : "A", 
+    },
+    {   
+        questionEl : "INDEX 2: Question 3",
+        choiceAEl : "a",
+        choiceBEl : "b",
+        choiceCEl : "c",
+        choiceDEl : "d",       
+        answer : "A", 
+    },
+    {   
+        questionEl : "INDEX 2: Question 3",
+        choiceAEl : "a",
+        choiceBEl : "b",
+        choiceCEl : "c",
+        choiceDEl : "d",       
+        answer : "A", 
+    },
+    {   
+        questionEl : "INDEX 2: Question 3",
+        choiceAEl : "a",
+        choiceBEl : "b",
+        choiceCEl : "c",
+        choiceDEl : "d",       
+        answer : "A", 
+    },
+    {   
+        questionEl : "INDEX 2: Question 3",
+        choiceAEl : "a",
+        choiceBEl : "b",
+        choiceCEl : "c",
+        choiceDEl : "d",       
+        answer : "A", 
+    },
+    {   
+        questionEl : "INDEX 2: Question 3",
+        choiceAEl : "a",
+        choiceBEl : "b",
+        choiceCEl : "c",
+        choiceDEl : "d",       
+        answer : "A", 
+    },
+    {   
+        questionEl : "INDEX 2: Question 3",
+        choiceAEl : "a",
+        choiceBEl : "b",
+        choiceCEl : "c",
+        choiceDEl : "d",       
+        answer : "A", 
+    },
+    {   
+        questionEl : "INDEX 2: Question 3",
+        choiceAEl : "a",
+        choiceBEl : "b",
+        choiceCEl : "c",
+        choiceDEl : "d",       
+        answer : "A", 
+    },
+    {   
+        questionEl : "INDEX 2: Question 3",
+        choiceAEl : "a",
+        choiceBEl : "b",
+        choiceCEl : "c",
+        choiceDEl : "d",       
+        answer : "A", 
+    },
+    {   
+        questionEl : "INDEX 2: Question 3",
+        choiceAEl : "a",
+        choiceBEl : "b",
+        choiceCEl : "c",
+        choiceDEl : "d",       
+        answer : "A", 
+    },
+    {   
+        questionEl : "INDEX 2: Question 3",
+        choiceAEl : "a",
+        choiceBEl : "b",
+        choiceCEl : "c",
+        choiceDEl : "d",       
+        answer : "A", 
+    },
+    {   
+        questionEl : "INDEX 2: Question 3",
+        choiceAEl : "a",
+        choiceBEl : "b",
+        choiceCEl : "c",
+        choiceDEl : "d",       
+        answer : "A", 
+    },
+    {   
+        questionEl : "INDEX 2: Question 3",
+        choiceAEl : "a",
+        choiceBEl : "b",
+        choiceCEl : "c",
+        choiceDEl : "d",       
+        answer : "A", 
+    },
+    {   
+        questionEl : "INDEX 2: Question 3",
+        choiceAEl : "a",
+        choiceBEl : "b",
+        choiceCEl : "c",
+        choiceDEl : "d",       
+        answer : "A", 
+    },
+    {   
+        questionEl : "INDEX 2: Question 3",
+        choiceAEl : "a",
+        choiceBEl : "b",
+        choiceCEl : "c",
+        choiceDEl : "d",       
+        answer : "A", 
+    },
+    {   
+        questionEl : "INDEX 2: Question 3",
+        choiceAEl : "a",
+        choiceBEl : "b",
+        choiceCEl : "c",
+        choiceDEl : "d",       
+        answer : "A", 
+    },
+    {   
+        questionEl : "INDEX 2: Question 3",
+        choiceAEl : "a",
+        choiceBEl : "b",
+        choiceCEl : "c",
+        choiceDEl : "d",       
+        answer : "A", 
+    },
 ];
+
+console.log(userChoice);
 
 choiceAEl.onclick = function (){
     userChoice = "A";
@@ -70,9 +212,11 @@ choiceDEl.onclick = function (){
 
 // ---- START QUIZ FUNCTION----- 
 // ACCEPTANCE CRITERIA: WHEN I click the start button THEN a timer starts and I am presented with a question
+startQuiz();
 function startQuiz (){
     //Event Listener: when start button clicked -> initiate these below
-    startBtnEl.addEventListener("click", () => {
+    startBtnEl.addEventListener("click", function(event) {
+        event.preventDefault();
         //hide the start button when clicked
         startBtnEl.disabled = true;
         startBtnEl.style.display="none";
@@ -85,7 +229,8 @@ function startQuiz (){
         setTime();
         });
 }
-// ---- END START QUIZ FUNCTION----- 
+// ---- END START QUIZ FUNCTION-----
+
 
 // ---- TIMER FUNCTION----- 
 // ACCEPTANCE CRITERIA: "THEN a timer starts and I am presented with a question"
@@ -96,9 +241,7 @@ function setTime(){
     timerEl.textContent = "Time Remaining: " + secondsLeft;
     currentScoreEl.textContent = "Your current score is " + theScore;
     
-    //TO DO: add a conditional that if I answer a question wrong, the timer subtracts time
-
-    if(secondsLeft === 0) {
+    if(secondsLeft <= 0) {
         clearInterval(timerInterval);
         endQuiz();
     }
@@ -108,12 +251,9 @@ function setTime(){
 // ---- END TIMER FUNCTION ----- 
 
 
-
-
-
-
 // NEXT QUESTION EVENT LISTENER -> When end user clicks on a question choice, the app moves to the next question
 //ACCEPTANCE CRITERIA: WHEN I answer a question THEN I am presented with another question
+clickNext();
 function clickNext () {
     choicesEl.forEach(function (choiceIndex) {
         choiceIndex.addEventListener("click", () => {
@@ -123,18 +263,12 @@ function clickNext () {
 };
 
 
-
-
-
-
 //ACCEPTANCE CRITERIA: WHEN all questions are answered or the timer reaches 0 THEN the game is over
-
 function nextQuestion(){
         var i = Math.floor(Math.random() * (cards.length));
         var correctAnswer = cards[i].answer;
-    
-
-    if(secondsLeft === 0 || cards.length === 0) {
+       
+    if(secondsLeft <= 0 || cards.length === 0) {
         endQuiz();
     }else{
         showQuestion(cards[i]);
@@ -157,13 +291,20 @@ function nextQuestion(){
         if (userChoice === correctAnswer) {
             theScore++;
         } else {
-            secondsLeft = secondsLeft - 5;
+            if(userChoice === ""){ //makes sure to not remove time when the code first runs since userChoice will be "" so it will !=== correctAnswer
+              return checkAnswer;  
+            }else{
+              secondsLeft = secondsLeft - 5;
+            };    
         };
     };
-    
+
+    console.log(userChoice);
+    console.log(correctAnswer);
+
     //End the game once there is only 1 item left in the cards array. 
         //If I wait until 0 items are in the array, the loop breaks and does not log the answer of the last random question
-    if(secondsLeft === 0 || cards.length === 1) {
+    if(secondsLeft <= 0 || cards.length === 10) {
         checkAnswer();
         endQuiz();
     }else{
@@ -180,35 +321,69 @@ function nextQuestion(){
 function endQuiz (){
     //hide the cards section & show the highScore 
     cardEl.style.display="none";    
-    viewHighScoreEl.style.display="none";  
     currentScoreEl.style.display="none";  
     highScoresEl.style.display="block";
     timerEl.style.display = "none";
-    console.log(theScore);
-    sendMessage();
+    person1El.style.display="none";
+    greetingEl.textContent = "Your time is done. Your score is: " + theScore + " points!";
 };
 
-function sendMessage(){
-    setTheScore();
-    function setTheScore() {
-        localStorage.setItem("theScore", theScore);
-    }
 
-    greetingEl.textContent = "Your time is done. Your score is: " + theScore;
-    myFormEl.addEventListener("submit", function(event) {
-        event.preventDefault();
-    });
+saveBtnEl.addEventListener("click", function (event){
+    event.preventDefault();
+    storePlayerData();
+   // getPlayerData();
 
-    getTheScore();
-    function getTheScore() {
-        var storedScore = localStorage.getItem("theScore");
-        if (storedScore === null) {
-            theScore = 0;
-        } else {
-            theScore = storedScore;
-        } 
+    function storePlayerData (){
+        var theInitials = userInputEl.value.trim();
+        var playerInfo = {
+            score: theScore.valueOf(),
+            initials: theInitials,
+        };
+
+
+        // the key is the name you designated in the "string" below so to add multiple arrays, change the name of the key
+        // make sure to look up how to alert the new user when an initial has been already stored in local storage
+
+        //random number generated to give players so if the same initials are used, it won't override any object existing in local Storage already
+        function getRandomInt(max){
+            return JSON.stringify(Math.floor(Math.random()*max));
+        }
+        
+        localStorage.setItem((theInitials + " - Player #" + (getRandomInt(10))  + " with a score of " + theScore), JSON.stringify(playerInfo));
+        person1El.style.display="block";
+        person1El.textContent=theInitials.value;
     };
-};
+});
 
-clickNext();
-startQuiz();
+
+viewAllHSEl.addEventListener("click", function (event){
+    event.preventDefault();
+
+    getPlayerData();
+    function getPlayerData (){
+        //Get all players data
+        for (var i = 0; i <localStorage.length; i++){
+            var key = localStorage.key(i);
+
+            var value = JSON.parse(localStorage.getItem(key));
+
+            console.log(key + ", Value: " + value);
+            
+            var list = document.createElement("li");
+            var textForList = document.createTextNode(key);
+            list.appendChild(textForList);
+            highScoresListEl.appendChild(list);
+        };
+    };
+});
+
+tryAgainBtnEl.addEventListener("click", function (event){
+    event.preventDefault();
+    location.reload();
+});
+
+clearHighScoresEl.addEventListener("click", function (event){
+    event.preventDefault();
+    localStorage.clear();
+});
